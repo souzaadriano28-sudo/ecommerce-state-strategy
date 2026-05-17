6994a381cf0802d907c5dbb5ea7329df48bda9bc
Adriano <souzaadriano28@yahoo.com.br>
2026-05-17 17:44:57 -0300

    Initial commit

---
+++ src/main/java/com/ecommerce/ordermanagement/EcommerceStateStrategyApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ordermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceStateStrategyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceStateStrategyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/ordermanagement/EcommerceStateStrategyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ordermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceStateStrategyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
